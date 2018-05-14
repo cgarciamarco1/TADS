5e178f98d2211e6aff968da4b7086dfd03f8e9ea
Fabio <zanifa2247@gmail.com>
2018-05-14 12:09:35 +0200

    Modificadas las funcionalidades para ver los pedidos en la mesa de trabajo para esconder los productos que no son pizzas y los pedidos que no contengan pizzas

--- html/20_mesa_trabajoM.php
+++ html/20_mesa_trabajoM.php
@@ -4,13 +4,13 @@
 	switch($_POST["OPC"]){
 		case "PEDIDOS":
 			//falta el control de la hora de activación
-			$SQL="SELECT ID, nombre_cliente, fecha_hora_activacion FROM pedido where estado = '1'";
+			$SQL="SELECT DISTINCT pedido.ID, nombre_cliente, fecha_hora_activacion FROM pedido, linea_pedido, productos WHERE pedido.ID = linea_pedido.pedido AND linea_pedido.idproducto = productos.ID AND estado = '1' AND tipo_producto = '1'";
 			echo get_Data_SQL($SQL, array(), $mysqli);
 			break;
 		
 		case "LINEAS_PEDIDO":
 			$idpedido = $_POST["idpedido"];
-			$SQL="SELECT nombre, cantidad, comentario, lista_ingredientes FROM linea_pedido, productos WHERE linea_pedido.idproducto = productos.ID AND pedido = $idpedido";
+			$SQL="SELECT nombre, cantidad, comentario, lista_ingredientes FROM linea_pedido, productos WHERE linea_pedido.idproducto = productos.ID AND pedido = $idpedido AND tipo_producto='1'";
 			echo get_Data_SQL($SQL, array(), $mysqli);
 			break;
 			
